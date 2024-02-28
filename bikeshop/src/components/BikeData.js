6699cb7ff9a4aca720dd87e6bfbce7e42a9942d9
Issa Nyale <me@email.com>
2024-02-29 00:50:09 +0300

    Added bike data

---
+++ bikeshop/src/components/BikeData.js
@@ -0,0 +1,82 @@
+import Cannondale from "../images/bike-big/cannondale.jpg";
+import Bianchi from "../images/bike-big/bianchi.jpg";
+import Ribble from "../images/bike-big/ribble.jpg";
+import BMC from "../images/bike-big/bmc.jpg";
+import Specialized from "../images/bike-big/specialized.jpg";
+import Vitus from "../images/bike-big/vitus.jpg";
+
+export const BIKE_DATA = [
+  [
+    {
+      name: "Cannondale Synapse",
+      price: "37",
+      img: Cannondale,
+      model: "Synapse",
+      make: "Carbon Fibre",
+      year: "2008",
+      gears: "4/5",
+      brakes: "Yes",
+    },
+  ],
+
+  [
+    {
+      name: "Bianchi",
+      price: "45",
+      img: Bianchi,
+      model: "Aria",
+      make: "Alluminium",
+      year: "2012",
+      gears: "4/5",
+      brakes: "Yes",
+    },
+  ],
+  [
+    {
+      name: "Ribble Ultra SL",
+      price: "30",
+      img: Ribble,
+      model: "Ultra SL",
+      make: "Magnesium alloy",
+      year: "2006",
+      gears: "4/5",
+      brakes: "Yes",
+    },
+  ],
+  [
+    {
+      name: "BMC Timemachine",
+      price: "35",
+      img: BMC,
+      model: "Timemachine",
+      make: "Aluminium alloy",
+      year: "2012",
+      gears: "4/5",
+      brakes: "Yes",
+    },
+  ],
+  [
+    {
+      name: "Specialized Roubaix",
+      price: "50",
+      img: Specialized,
+      model: "Roubaix",
+      make: "Magnesium",
+      year: "2006",
+      gears: "4/5",
+      brakes: "Yes",
+    },
+  ],
+  [
+    {
+      name: "Vitus Zenium",
+      price: "25",
+      img: Vitus,
+      model: "Zenium",
+      make: "Carbon",
+      year: "2008",
+      gears: "4/5",
+      brakes: "Yes",
+    },
+  ],
+];
