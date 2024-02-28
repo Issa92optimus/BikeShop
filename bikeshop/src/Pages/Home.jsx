9bddfbf910c2434bf60a6753180fb01954bc2199
Issa Nyale <me@email.com>
2024-02-28 23:17:48 +0300

    Imported Testimonials at Home.jsx

--- bikeshop/src/Pages/Home.jsx
+++ bikeshop/src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import PlanTrip from "../components/PlanTrip";
 import Footer from "../components/Footer";
+import Testimonials from "../components/Testimonials";
 
 function Home() {
     return(
         <>
             <PlanTrip />
             <Footer />
+            <Testimonials />
         </>
     );
 }
