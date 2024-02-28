cec518205954f1590c3819a55f4eeed069f1deab
Issa Nyale <me@email.com>
2024-02-28 19:05:21 +0300

    Added all Images

---
+++ bikeshop/src/Pages/About.jsx
@@ -0,0 +1,17 @@
+import { IconPhone } from "@tabler/icons-react";
+
+function About() {
+    return (
+        <>
+            <section className="about-page">
+                <HeroPages name="About">
+                    <div className="container">
+                        <div className="about-main">
+                            
+                        </div>
+                    </div>
+                </HeroPages>
+            </section>
+        </>
+    )
+}
