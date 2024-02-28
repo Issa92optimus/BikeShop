057b0e5d5a9d5142cf511fea7b5c985a585df0c2
Issa Nyale <me@email.com>
2024-02-28 19:50:10 +0300

    Done with HeroPages

--- bikeshop/src/components/HeroPages.jsx
+++ bikeshop/src/components/HeroPages.jsx
@@ -1,7 +1,21 @@
 import { Link } from "react-router-dom";
 
-function HeroPages() {
-
+function HeroPages({ name }) {
+    return (
+        <>
+            <section className="hero-pages">
+                <div className="hero-pages_overlay"></div>
+                <div className="container">
+                    <div className="hero-pages_text">
+                        <h3>{name}</h3>
+                        <p>
+                            <Link to="/">Home</Link> / {name}
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default HeroPages;
