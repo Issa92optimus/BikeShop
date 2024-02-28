701b604158a3d4ee61eaf60ca7d7acef02436bbc
Issa Nyale <me@email.com>
2024-02-29 01:16:17 +0300

    Done with download

---
+++ bikeshop/src/components/Download.jsx
@@ -0,0 +1,26 @@
+import Img1 from "../images/download/appstore.svg";
+import Img2 from "../images/download/googleapp.svg";
+
+function Download() {
+  return (
+    <>
+      <section className="download-section">
+        <div className="container">
+          <div className="download-text">
+            <h2>Download our app to get most out of it</h2>
+            <p>
+              Get our mobile application from either Google Playstore or Applestore
+              to have both expereince via mobile phones and through using our website in your laptops & computers.
+            </p>
+            <div className="download-text__btns">
+              <img alt="download_img" src={Img2} />
+              <img alt="download_img" src={Img1} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Download;
