231514e75400aadd411a9b4d649a2cc686f3b42e
Issa Nyale <me@email.com>
2024-02-28 23:20:12 +0300

    Set route at App.js

--- bikeshop/src/App.js
+++ bikeshop/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from './Pages/Contact';
 import Models from './Pages/Models';
 import Team from './Pages/Team';
+import TestimonialsPage from './Pages/TestimonialsPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="contact" element={<Contact />} />
           <Route path="models" element={<Models />} />
           <Route path="team" element={<Team />} />
+          <Route path="testimonials" element={<TestimonialsPage />} />
         </Routes>
       </Router>
     </>
