e6a811b37d7a13c67f923fdc421a56fe943c8dcd
Issa Nyale <me@email.com>
2024-02-29 00:59:18 +0300

    Done with Hero

---
+++ bikeshop/src/components/Hero.jsx
@@ -0,0 +1,84 @@
+import { Link } from "react-router-dom";
+import BgShape from "../images/hero/hero-bg.png";
+import HeroBike from "../images/hero/main-car.png";
+import { useEffect, useState } from "react";
+import { IconChevronDown, IconCircleCheck } from "@tabler/icons-react";
+
+function Hero() {
+  const [goUp, setGoUp] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: (0, 0), behavior: "smooth" });
+  };
+
+  const bookBtn = () => {
+    document
+      .querySelector("#booking-section")
+      .scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const onPageScroll = () => {
+      if (window.pageYOffset > 600) {
+        setGoUp(true);
+      } else {
+        setGoUp(false);
+      }
+    };
+    window.addEventListener("scroll", onPageScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onPageScroll);
+    };
+  }, []);
+  return (
+    <>
+      <section id="home" className="hero-section">
+        <div className="container">
+          <img className="bg-shape" src={BgShape} alt="bg-shape" />
+          <div className="hero-content">
+            <div className="hero-content__text">
+              <h4>Plan your trip now</h4>
+              <h1>
+                Save <span>much</span> with bike-rental.
+              </h1>
+              <p>
+                Rent or buy the bike of your dreams and adventure
+                around everywhere to feel the joy that comes with cycling.
+              </p>
+              <div className="hero-content__text__btns">
+                <Link
+                  onClick={bookBtn}
+                  className="hero-content__text__btns__book-ride"
+                  to="/"
+                >
+                  Book Ride &nbsp; <IconCircleCheck />
+                </Link>
+                <Link className="hero-content__text__btns__learn-more" to="/">
+                  Learn More &nbsp; <IconChevronDown />
+                </Link>
+              </div>
+            </div>
+
+            {/* img */}
+            <img
+              src={HeroBike}
+              alt="bike-img"
+              className="hero-content__bike-img"  
+            />
+          </div>
+        </div>
+
+        {/* page up */}
+        <div
+          onClick={scrollToTop}
+          className={`scroll-up ${goUp ? "show-scroll" : ""}`}
+        >
+          ^
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
